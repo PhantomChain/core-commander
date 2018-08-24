ef646efa9f4095a6f2c2fdfeb7c8cd916b5b4baf
filip <filip@phantom.org>
2018-08-24 19:21:15 +0200

    rename

--- actions/core/configure/database.sh
+++ actions/core/configure/database.sh
@@ -8,14 +8,14 @@ core_configure_database ()
 
     . "$envFile"
 
-    read -p "Enter the database host, or press ENTER for the default [$ARK_DB_HOST]: " inputHost
-    read -p "Enter the database port, or press ENTER for the default [$ARK_DB_PORT]: " inputPort
-    read -p "Enter the database username, or press ENTER for the default [$ARK_DB_USERNAME]: " inputUsername
-    read -p "Enter the database password, or press ENTER for the default [$ARK_DB_PASSWORD]: " inputPassword
-    read -p "Enter the database name, or press ENTER for the default [ark_${CORE_NETWORK}]: " inputDatabase
+    read -p "Enter the database host, or press ENTER for the default [$PHANTOM_DB_HOST]: " inputHost
+    read -p "Enter the database port, or press ENTER for the default [$PHANTOM_DB_PORT]: " inputPort
+    read -p "Enter the database username, or press ENTER for the default [$PHANTOM_DB_USERNAME]: " inputUsername
+    read -p "Enter the database password, or press ENTER for the default [$PHANTOM_DB_PASSWORD]: " inputPassword
+    read -p "Enter the database name, or press ENTER for the default [phantom_${CORE_NETWORK}]: " inputDatabase
 
     if [[ ! -z "$inputHost" ]]; then
-        sed -i -e "s/ARK_DB_HOST=$ARK_DB_HOST/ARK_DB_HOST=$inputHost/g" "$envFile"
+        sed -i -e "s/PHANTOM_DB_HOST=$PHANTOM_DB_HOST/PHANTOM_DB_HOST=$inputHost/g" "$envFile"
     fi
 
     if [[ ! -z "$inputPort" ]]; then
@@ -23,17 +23,17 @@ core_configure_database ()
     fi
 
     if [[ ! -z "$inputUsername" ]]; then
-        sed -i -e "s/ARK_DB_USERNAME=$ARK_DB_USERNAME/ARK_DB_USERNAME=$inputUsername/g" "$envFile"
+        sed -i -e "s/PHANTOM_DB_USERNAME=$PHANTOM_DB_USERNAME/PHANTOM_DB_USERNAME=$inputUsername/g" "$envFile"
     fi
 
     if [[ ! -z "$inputPassword" ]]; then
-        sed -i -e "s/ARK_DB_PASSWORD=$ARK_DB_PASSWORD/ARK_DB_PASSWORD=$inputPassword/g" "$envFile"
+        sed -i -e "s/PHANTOM_DB_PASSWORD=$PHANTOM_DB_PASSWORD/PHANTOM_DB_PASSWORD=$inputPassword/g" "$envFile"
     fi
 
     if [[ ! -z "$inputDatabase" ]]; then
-        sed -i -e "s/ARK_DB_DATABASE=$ARK_DB_DATABASE/ARK_DB_DATABASE=$inputDatabase/g" "$envFile"
+        sed -i -e "s/PHANTOM_DB_DATABASE=$ARK_DB_DATABASE/PHANTOM_DB_DATABASE=$inputDatabase/g" "$envFile"
     else
-        sed -i -e "s/ARK_DB_DATABASE=$ARK_DB_DATABASE/ARK_DB_DATABASE=ark_${CORE_NETWORK}/g" "$envFile"
+        sed -i -e "s/PHANTOM_DB_DATABASE=$ARK_DB_DATABASE/PHANTOM_DB_DATABASE=ark_${CORE_NETWORK}/g" "$envFile"
     fi
 
     . "$envFile"
--- menus/core/help.sh
+++ menus/core/help.sh
@@ -6,15 +6,15 @@ menu_manage_core_help ()
 
     divider
 
-    text_yellow "    Update Ark (U)"
-    text_white "    update Ark Core if a new version is available."
+    text_yellow "    Update PHANTOM (U)"
+    text_white "    update PHANTOM core if a new version is available."
     echo
 
-    text_yellow "    Uninstall Ark (P)"
-    text_white "    uninstall Ark Core from your system."
+    text_yellow "    Uninstall PHANTOM (P)"
+    text_white "    uninstall PHANTOM Core from your system."
     echo
 
-    text_yellow "    Configure Ark (C)"
+    text_yellow "    Configure PHANTOM (C)"
     text_white "    lets you select a network configuration and automatically creates the database for the config you chose."
     echo
 
@@ -23,7 +23,7 @@ menu_manage_core_help ()
 
     divider
 
-    text_blue "    For more information head on over to https://docs.ark.io/"
+    text_blue "    For more information head on over to https://docs.phantom.io/"
     echo
 
     press_to_continue
